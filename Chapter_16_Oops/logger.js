b79c27e8b9bf746ef44dede90cc0ad2a31c6d3b0
Deepti <deeptiraninayak5@gmail.com>
2026-04-07 17:33:25 +0530

    Export & Import

---
+++ Chapter_16_Oops/logger.js
@@ -0,0 +1,9 @@
+// Default Export -> Export one Main thing
+
+export default function log(message){
+    console.log("[LOG] " + message)
+}
+
+export function log2(message){
+    console.log("[Logs] " + message)
+}
